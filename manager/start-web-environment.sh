0e2eefee0537a8cd609db4dc2d3d431bcc78c2f2
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2016-10-01 11:01:13 +0200

    Add systemd service scripts to start browser and server on startup

--- manager/start-web-environment.sh
+++ manager/start-web-environment.sh
@@ -3,4 +3,4 @@
 INTERNAL_PORT=5001
 REMOTE_DEBUGGING_PORT=9222
 
-/usr/bin/xvfb-run chromium-browser --remote-debugging-port=$REMOTE_DEBUGGING_PORT --app=http://localhost:$INTERNAL_PORT/radio
+/usr/bin/xvfb-run -a chromium-browser --remote-debugging-port=$REMOTE_DEBUGGING_PORT --app=http://localhost:$INTERNAL_PORT/manager --user-data-dir=../chromium-user-data
