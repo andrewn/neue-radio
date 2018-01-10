c12639d13eeba832b4d3a1eb34f0f5d5f0062121
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-01-10 12:12:59 +0100

    Subsequent calls to `play(url)` will immediately play next media

--- youtube/youtube.js
+++ youtube/youtube.js
@@ -6,13 +6,18 @@ class YouTube {
   play(url) {
     const videoId = new URL(url).searchParams.get('v');
 
-    this.player = new YT.Player(this.element, {
-      videoId,
-      events: {
-        onReady: () => this.player.playVideo()
-        // 'onStateChange': onPlayerStateChange
-      }
-    });
+    if (this.player == null) {
+      this.player = new YT.Player(this.element, {
+        videoId,
+        events: {
+          onReady: () => this.player.playVideo()
+          // 'onStateChange': onPlayerStateChange
+        }
+      });
+    } else {
+      this.player.cueVideoById(videoId);
+      this.player.playVideo();
+    }
   }
   stop() {
     this.player.stopVideo();
