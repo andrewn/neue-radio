a7c351c4c8cd570df92077e90fd53f0ad16a9f87
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-01-18 13:53:24 +0100

    Format code

--- services/manager/lib/apps/index.js
+++ services/manager/lib/apps/index.js
@@ -4,15 +4,13 @@ const path = require('path');
 const isDirectory = ({ path }) => fs.lstatSync(path).isDirectory();
 const isNotHiddenDirectory = ({ path }) => path[0] !== '.';
 
-const getDirectories = source => (
-  fs.readdirSync(source)
+const getDirectories = source =>
+  fs
+    .readdirSync(source)
     .map(name => ({ path: path.join(source, name), name }))
     .filter(isDirectory)
-    .filter(isNotHiddenDirectory)
-);
+    .filter(isNotHiddenDirectory);
 
-const appNamesToMount = appPath => (
-  getDirectories(appPath)
-);
+const appNamesToMount = appPath => getDirectories(appPath);
 
 module.exports = appNamesToMount;
