030784e71745c32c379459ed76d0a3af7276b918
Dan Nuttall <pixelblend@gmail.com>
2018-01-30 14:09:17 +0200

    Add single app support to manager
    The default setting for manager is to load every app it can find in a
    given path.
    
    If a `APP_PATH` environment variable is set, the manager will attempt to
    mount just that single application.

--- services/manager/lib/apps/index.js
+++ services/manager/lib/apps/index.js
@@ -4,13 +4,19 @@ const path = require('path');
 const isDirectory = ({ path }) => fs.statSync(path).isDirectory();
 const isNotHiddenDirectory = ({ path }) => path[0] !== '.';
 
-const getDirectories = source =>
+const findApps = source => (
   fs
     .readdirSync(source)
     .map(name => ({ path: path.join(source, name), name }))
     .filter(isDirectory)
-    .filter(isNotHiddenDirectory);
+    .filter(isNotHiddenDirectory)
+);
 
-const appNamesToMount = appPath => getDirectories(appPath);
+const singleApp = appPath => (
+  [{ path: appPath, name: path.posix.basename(appPath) }]
+    .filter(isDirectory)
+    .filter(isNotHiddenDirectory)
+);
 
-module.exports = appNamesToMount;
+module.exports.findApps = findApps;
+module.exports.singleApp = singleApp;
