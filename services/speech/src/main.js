6e0b2cb56e266b59bc34d2f00f3996607029c1d7
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-02-19 14:10:27 +0100

    WS_HOST is optional, set to system IP if not provided

--- services/speech/src/main.js
+++ services/speech/src/main.js
@@ -1,14 +1,20 @@
+const ip = require("ip");
+
+const log = require("debug")("speechd:main");
+
 const connectToSpeechd = require("./speechd");
 const connectToBroker = require("./ws");
 const handleMessage = require("./handleMessage");
 
-const WS_HOST = process.env.WS_HOST;
+const WS_HOST = process.env.WS_HOST || ip.address();
 const WS_PORT = process.env.WS_PORT;
 
 const main = async () => {
   try {
     const speech = await connectToSpeechd({ autoSpawn: true });
     const broker = await connectToBroker(WS_HOST, WS_PORT);
+
+    log("Connecting to broker with WS_HOST: ", WS_HOST, " WS_PORT:", WS_PORT);
 
     broker.on("message", handleMessage(speech, broker));
     broker.on("close", () => process.exit());
