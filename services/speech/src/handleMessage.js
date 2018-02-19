0ac928e9837c4c353a8707d61a69213c47cd5b9d
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-02-19 15:15:23 +0100

    We use SLASHES around these parts, not DOTS.

--- services/speech/src/handleMessage.js
+++ services/speech/src/handleMessage.js
@@ -1,14 +1,14 @@
 module.exports = (speech, broker) => async (topic, payload) => {
   try {
     switch (topic) {
-      case "speech.command.speak":
+      case "speech/command/speak":
         if (payload.voiceType) {
           await speech.setVoiceType(payload.voiceType);
         }
         speech.speak(payload.utterance);
         break;
-      case "speech.command.list-voices":
-        broker.send("speech.event.available-voices", {
+      case "speech/command/list-voices":
+        broker.send("speech/event/available-voices", {
           voices: await speech.listVoices()
         });
         break;
