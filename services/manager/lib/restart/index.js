6a23fefdd468f564704a9e45a788dc1d478d99a1
Dan Nuttall <pixelblend@gmail.com>
2018-03-09 17:28:49 +0200

    Reload internal apps when manager service onlines

--- services/manager/lib/restart/index.js
+++ services/manager/lib/restart/index.js
@@ -2,8 +2,16 @@ const createWebsocket = require('websocket').default;
 const shutdown = () => process.exit(0);
 
 const listenForRestart = () => {
-  createWebsocket()
-    .subscribe('manager/command/restart', shutdown);
+  const ws = createWebsocket();
+
+  ws.ready.then(() => {
+    // reload page running internal apps from the manager
+    setTimeout(() => {
+      ws.publish({ topic: 'manager-web/command/restart' });
+    }, 1000);
+  });
+
+  ws.subscribe('manager/command/restart', shutdown);
 };
 
 module.exports = listenForRestart;
