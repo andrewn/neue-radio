473cf02f5cdd872982c84b16e8aeb03323fe61dd
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-03-13 12:11:56 +0100

    Extracts actions out for switch to own functions

--- services/speech/src/handleMessage.js
+++ services/speech/src/handleMessage.js
@@ -1,21 +1,35 @@
+const actions = {
+  speak: async (speech, broker, topic, payload) => {
+    if (payload.voiceType) {
+      await speech.setVoiceType(payload.voiceType);
+    }
+
+    speech.speak(payload.utterance);
+  },
+  listVoices: async (speech, broker) => {
+    broker.publish({
+      topic: "speech/event/availablevoices",
+      payload: {
+        voices: await speech.listVoices()
+      }
+    });
+  }
+};
+
 module.exports = (speech, broker) => async ({ topic, payload }) => {
+  let action = null;
+
   try {
     switch (topic) {
       case "speech/command/speak":
-        if (payload.voiceType) {
-          await speech.setVoiceType(payload.voiceType);
-        }
-        speech.speak(payload.utterance);
+        action = actions.speak;
         break;
       case "speech/command/listvoices":
-        broker.publish({
-          topic: "speech/event/availablevoices",
-          payload: {
-            voices: await speech.listVoices()
-          }
-        });
+        action = actions.listVoices;
         break;
     }
+
+    action(speech, broker, topic, payload);
   } catch (e) {
     console.log("Error handling action", e);
   }
