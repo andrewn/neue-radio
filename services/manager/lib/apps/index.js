8b6a1faa5a74edd296b32f7bbd94f8082db798aa
Dan Nuttall <pixelblend@gmail.com>
2018-03-19 19:05:36 +0200

    Resolve paths when looking for apps to run
    This allows you to use relative paths at the command line, e.g.:
    
    APP_PATH=../apps/app-name npm start

--- services/manager/lib/apps/index.js
+++ services/manager/lib/apps/index.js
@@ -4,18 +4,20 @@ const path = require('path');
 const isDirectory = ({ path }) => fs.statSync(path).isDirectory();
 const isNotHiddenDirectory = ({ path }) => path[0] !== '.';
 
-const findApps = source => (
-  fs
-    .readdirSync(source)
-    .map(name => ({ path: path.join(source, name), name }))
+const findApps = rootPath => source => {
+  const fullPath = path.resolve(rootPath, source);
+
+  return fs
+    .readdirSync(fullPath)
+    .map(name => ({ path: path.join(fullPath, name), name }))
     .filter(isDirectory)
     .filter(isNotHiddenDirectory)
-);
+};
 
-const knownApps = appPath => (
+const knownApps = rootPath => appPath => (
   appPath
     .split(':')
-    .map(a => ({ path: a, name: path.posix.basename(a) }))
+    .map(a => ({ path: path.resolve(rootPath, a), name: path.posix.basename(a) }))
     .filter(isDirectory)
     .filter(isNotHiddenDirectory)
 );
