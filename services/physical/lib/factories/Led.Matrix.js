b6946b5110603c7fdd854563c514884bcf0a6a9d
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2018-04-18 22:07:23 +0200

    Adds Led.Matrix component to physical service

---
+++ services/physical/lib/factories/Led.Matrix.js
@@ -0,0 +1,18 @@
+const five = require('johnny-five');
+
+module.exports = function(spec, routable) {
+  const id = spec.id;
+  const config = spec.config;
+
+  const matrix = five.Led.Matrix(Object.assign({ id: id }, config));
+
+  routable.on('request', function(req) {
+    if (req.data && Array.isArray(req.data)) {
+      try {
+        matrix.draw(req.data);
+      } catch (error) {
+        console.error(id, error);
+      }
+    }
+  });
+};
